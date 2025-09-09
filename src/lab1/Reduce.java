e61418f63cd2a9fcea6992683c3c89fb57d25e5c
wuqingyiWANG <wuqingyiwang@gmail.com>
2025-09-09 14:18:23 -0400

    Create a Reduce class that prints how many steps it takes to reach 0 starting at a number n

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,4 +1,36 @@
 package lab1;
 
 public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+            else if (n % 2 == 1) {
+                n -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    public static int reduce() {
+        int count = 0;
+        int n = 100;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+            else if (n % 2 == 1) {
+                n -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
 }
