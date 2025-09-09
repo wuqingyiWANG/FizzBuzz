0b7189c5eb0d0c375c694176ba89292e6b3543e3
sooodagreen <1wangche5@hdsb.ca>
2025-09-09 13:54:06 -0400

    simplified the multiples method and updated after test

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,19 +1,17 @@
 package lab1;
-import java.util.*;
 
 public class Multiples {
     public static void main(String[] args) {
-        int count = multiples();
-        System.out.println(count);
-
+        System.out.println(multiples(1000, 3, 5));
     }
-    public static int multiples() {
-        List<Integer> multiples = new ArrayList<>();
-        for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                multiples.add(i);
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
             }
         }
-        return multiples.size();
+        return count;
     }
 }
